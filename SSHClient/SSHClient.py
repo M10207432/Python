a2c1e79f1ae14a56a6a5dfb7e856620bd8ac9e61
M10207432 <M10207432@mail.ntust.edu.tw>
2015-03-20 20:46:25 +0800

    Ask acoount & password

--- SSHClient/SSHClient.py
+++ SSHClient/SSHClient.py
@@ -2,18 +2,23 @@ import paramiko
 import time
 import re
 
-def main():
-    #Connect SSH server
-    ssh=paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect("140.118.172.121",22,"m10207432","m10207432")
 
+account=raw_input("Account:")
+passwd=raw_input("Password:")
+
+#Connect SSH server
+ssh=paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect("140.118.172.121",22,account,passwd)
+
+def SendCMD():
     chan=ssh.invoke_shell()
     print chan.recv(9999)
     chan.send('\n')
     time.sleep(1)
     result=chan.recv(9999)
     insidecmd=0
+    
     #Command Script
     try:
         while True:
@@ -39,6 +44,16 @@ def main():
                 print (re.compile(r'\x1b[^m]*m')).sub('', i)
     except:
         ssh.close()
+
+def main():
+    '''
+    sftp=paramiko.SFTPClient.from_transport(ssh)
+    remotepath='/home/m10207432/upload.txt'
+    localpath='/upload.txt'
+    sftp.put(localpath,remotepath)
+    ssh.close()
+    '''
+    SendCMD()
     
 if __name__=="__main__":
     main()
