9f3aaed0accd33c57e1b998354fb66751f4f2145
M10207432 <M10207432@mail.ntust.edu.tw>
2015-05-06 21:33:04 +0800

    SSH upload & download

--- SSHClient/SSHClient.py
+++ SSHClient/SSHClient.py
@@ -52,8 +52,9 @@ def SendCMD():
 
 '''====================================
         remotepath
+(remotedir should be root by the ssh server, or create by self)
 ===================================='''
-def SSHUpload(remotedir,localfile):
+def SSHUpload(remotedir, remotefile, localfile):
     Conn_sftp=paramiko.Transport((ip, port))
     Conn_sftp.connect(username=account, password=passwd)
 
@@ -63,21 +64,30 @@ def SSHUpload(remotedir,localfile):
     except IOError:
         sftp.mkdir(remotedir)  # Create remote_path
         sftp.chdir(remotedir)
-    sftp.put(localfile, remotedir)    # At this point, you are in remote_path in either case
+    sftp.put(localfile, remotefile)    # At this point, you are in remote_path in either case
     Conn_sftp.close()
     
-def SSHdlowload(remotefile,localfile):
+def SSHdlowload(remotefile, localdir, localfile):
     Conn_sftp=paramiko.Transport((ip, port))
     Conn_sftp.connect(username=account, password=passwd)
 
+    try:
+        os.chdir(localdir)
+    except:
+        os.mkdir(localdir)
+        os.chdir(localdir)
+    
     sftp = paramiko.SFTPClient.from_transport(Conn_sftp)
-    sftp.get(localfile, remotefile)    # At this point, you are in remote_path in either case
+    #sftp.getfo(remotefile, )
+    sftp.get(remotefile,localfile)    # At this point, you are in remote_path in either case
     Conn_sftp.close()
     
 def main():
     
-    SSHUpload('./sshtest2', './sshtest3.txt')#remotedir, localfile
+    #SSHUpload('./sshtest', 'GE2_v1.jpg', './file/GE2_v1.jpg')#remotedir, remotefilename, localfile
     
+    SSHdlowload('./sshtest/GE2_v1.jpg','./Download ', 'GE2_v1.jpg')#remotedir, localdir, localfile
+
     #SendCMD()
     
 if __name__=="__main__":
