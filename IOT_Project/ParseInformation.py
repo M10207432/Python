51653ff832922652b51a61a98832ae3d4d3af6f8
M10207432 <M10207432@mail.ntust.edu.tw>
2015-05-22 14:21:02 +0800

    Add Client socket

--- IOT_Project/ParseInformation.py
+++ IOT_Project/ParseInformation.py
@@ -1,73 +1,38 @@
-import urllib2
-from xml.dom import minidom
-from lxml import etree
-import HTMLParser
-from lxml.html import fromstring, tostring
+from socket import socket, AF_INET, SOCK_STREAM
+import msvcrt as m
+import flask
+import flask.views
+from flask import request
 
-weatherweb='http://opendata.cwb.gov.tw/opendata/MFC/F-C0032-001.xml'
-Constellationweb='http://astro.click108.com.tw/'
+import sys, os
 
-def ParseWeather():
-    w_web=urllib2.urlopen(weatherweb)
-    xmldoc = minidom.parse(w_web)
-    root = xmldoc.documentElement
+os.sys.path.append("./include")
+import IOT_Frame as MainFrame
+import IOT_Parse as MainParse
 
-    node = root.getElementsByTagName('location')
-    for i in range(len(node)):
-        itemname=node[i].getElementsByTagName('locationName')
-        itempara=node[i].getElementsByTagName('parameterName')
-        itemtime=node[i].getElementsByTagName('startTime')
-        print "================================================="
+IP='localhost'
+port = 8000
 
-        name = itemname[0].toxml()
-        w_status = itempara[0].toxml()
-        T = itempara[3].toxml()
-        time = itemtime[0].toxml()
-        
-        print name, w_status, T
-        print time
-        
-def ParseConstellation(num):
     
-    '''======================================================
-                        First Choose Constellation
-    ======================================================'''
-    webdata=urllib2.urlopen(Constellationweb)
-    textcode=HTMLParser.HTMLParser()
+def Client_SendData(data):
+    s= socket(AF_INET, SOCK_STREAM)
+    s.connect((IP, port))
+    s.settimeout(0.1)
 
-    webHTMLdata=webdata.read()
-    ET=etree.HTML(webHTMLdata)
-
-    #0~11 Constellation <no tbody>
-    tr=ET.xpath('body/div/div/table/tr/td/div/div/div/div/div/ul/li')[num]
-    
-    v=etree.tostring(tr)
-    v=textcode.unescape(v)
-    web=v[v.find('<a href=')+9:v.find(' style=')-1]
-    C_name=v[v.find(' title=')+8:v.find('>',v.find(' title='))-1]
-    
-    print C_name
-    #print web
-
-    '''======================================================
-                        Final Parse information
-    ======================================================'''
-    webdata=urllib2.urlopen(web)
-    textcode=HTMLParser.HTMLParser()
-
-    webHTMLdata=webdata.read()
-    ET=etree.HTML(webHTMLdata)
-    
-    #0->Luck num, 1->Lucky color, 2->Lucky site, 3->Lucky time
-    tr=ET.xpath('body/div/table/tr/td/div/div/div/div/div/h4')[1]
-
-    v=etree.tostring(tr)
-    v=textcode.unescape(v)
-    print v
-    
+    #RECV -> SEND
+    msg=s.recv(1024)
+    s.send(data)
+          
 def main():
-    ParseWeather()
-    ParseConstellation(2)#0~11
+    '''
+    #Client_SendData(str(1)+'\n')
+    
+    MainFrame.Hello()
+    
+    ParserObj=MainParse.WebParse()
+    ParserObj.ParseWeather()
+    #data=ParserObj.ParseConstellation(2)#0~11
+    '''
     
 if __name__=="__main__":
     main()
