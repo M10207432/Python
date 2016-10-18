760c6c5a89b649a350b8988ef83c3dca421bbd71
M10207432 <M10207432@mail.ntust.edu.tw>
2016-10-18 22:15:42 +0800

    bat setting & Python list insert to C lib

--- FaceDetect/Include/C++/MachineLearn.cpp
+++ FaceDetect/Include/C++/MachineLearn.cpp
@@ -1,11 +1,45 @@
 #include "MachineLearn.h"
 
-int AddOne(int n){
-    return n+1;
+/*======================
+	Global Variable
+======================*/
+
+double input[InputNum];
+double weight[InputNum+HiddenNum][HiddenNum];
+
+/*======================
+		Function
+======================*/
+void weightEvaluate(int start, int end_id, struct _hiddennode* node){
+	
+	int target_id=node->id;
+	double output=0;
+	
+	for(int id=start; id<=end_id; id++){
+		output+=weight[id][target_id]*input[id];
+	}
+	
+	output+=node->threshold;
+	
+	node->output=output;	
 }
 
-int Sqrt(int n){
-    return n*n;
+
+double GetInput(PyObject* list){
+	int i, n;
+    double total = 0;
+    PyObject *item;
+
+    n = PyList_Size(list);
+    if (n < 0)
+        return -1; 
+    for (i = 0; i < n; i++) {
+        item = PyList_GetItem(list, i); 
+        if (!PyInt_Check(item)) 
+			continue; 
+        total += PyInt_AsLong(item);
+    }
+    return total;
 }
 
 double Add(double n1, double n2){
--- FaceDetect/Include/C++/MachineLearn.h
+++ FaceDetect/Include/C++/MachineLearn.h
@@ -1,8 +1,43 @@
 #include "Python.h"
 
-int AddOne(int n);
-int Sqrt(int n);
+/*======================
+	Configuration
+======================*/
+
+#define InputNum 50
+#define HiddenNum 50
+#define OutputNum 50
+
+/*======================
+		Structure
+======================*/
+
+typedef struct _hiddennode{
+	int id;			//HiddenNode id
+	int layer_id;		//HiddenNode Layer id
+	
+	double threshold;	
+	
+	double output;
+	double Err;
+	
+	struct _hiddennode* next;
+	struct _hiddennode* pre;
+}HiddenNode;
+
+/*======================
+	Function Prototype
+======================*/
+
+double GetInput(PyObject* list);
+void weightEvaluate(int start, int end_id, struct _hiddennode* node);
+
+
+/*======================
+		Test
+======================*/
 double Add(double n1, double n2);
 float Div(float n, float div);
+
 
 PyObject* SqrtInPyObj(PyObject* obj);
