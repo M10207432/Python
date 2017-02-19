40c80e97bde59e2bb9ad8bdd5c70467067f0e86e
M10207432 <M10207432@mail.ntust.edu.tw>
2017-02-20 00:49:25 +0800

    Add search Stock number

--- YTDownload/Stock.py
+++ YTDownload/Stock.py
@@ -1,44 +1,42 @@
 import os,sys
+
 import numpy as np
 from numpy import *
 import matplotlib.pyplot as plt
+
 from datetime import datetime
 import re
+import requests
+from bs4 import BeautifulSoup
 
 from grs import Stock
 from grs import TWSENo
 
-def test():
-	dataset=[[1,6],[8,6]]
-	dataMat=mat(dataset).T
-	plt.scatter(dataMat[0],dataMat[1],c='red',marker='o')	
-	
-	X=np.linspace(-2,2,100)
-	Y=2.8*X+9
-	
-	plt.plot(X,Y)
-	plt.show()
-	
-def stockGet():
-	
-	stock = Stock('2330',3)
-	time_list=[]
-	stock_list=[]
-	
-	print len(stock.raw)                 
-	for i in stock.raw:
-		
-		time_obj=re.sub(r"\d+/",str(int(i[0][:i[0].find('/')])+1911)+"/",i[0],1)
-		time=datetime.strptime(time_obj,"%Y/%m/%d")
+class StockObj():
+        def __init__ (self):
+                print "Stock"
+                
+        def showStocknum(self):
+                twse_no = TWSENo()
+                for i in twse_no.all_stock_no:
+                        print i,twse_no.searchbyno(i)[i]
+                
+        def stockGet(self,num):
+                
+                stock = Stock(num,3)
+                time_list=[]
+                stock_list=[]
+                
+                print len(stock.raw)                 
+                for i in stock.raw:
+                        
+                        time_obj=re.sub(r"\d+/",str(int(i[0][:i[0].find('/')])+1911)+"/",i[0],1)
+                        time=datetime.strptime(time_obj,"%Y/%m/%d")
+                        time_list.append(time)
+                        stock_list.append(i[6])
 
-		#print time,i[6]
-		time_list.append(time)
-		stock_list.append(i[6])
-
-	plt.plot(time_list,stock_list)
-	plt.show()
-	'''
-	twse_no=TWSENo()
-	for n in twse_no.all_stock_name:
-		print n
-	'''
+                plt.plot(time_list,stock_list)
+                plt.show()
+if __name__=="__main__":
+        s=StockObj()
+        s.showStocknum()  
