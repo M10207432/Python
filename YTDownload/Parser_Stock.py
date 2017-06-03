0db8b1bc973221e0c56def28b7aee36899129837
M10207432 <M10207432@mail.ntust.edu.tw>
2017-06-03 22:16:33 +0800

    Create StockNo Data structure & Dump as sequence

--- YTDownload/Parser_Stock.py
+++ YTDownload/Parser_Stock.py
@@ -3,6 +3,9 @@ import requests
 import urllib2
 import re
 import datetime
+from collections import *
+import pickle
+
 """========================
 0	"日期"
 1	"成交股數"
@@ -14,13 +17,18 @@ import datetime
 7	"漲跌價差"
 8	"成交筆數"
 ========================"""
+
 class Stock_Info():
     
     def __init__(self, stock_url, payload):
         self.stock_url = stock_url
         self.payload = payload
-
+        
+        self.stockInfo = OrderedDict()
+        
     def stockGet(self, stockNo, date):
+        self.__checkStockNo__(stockNo);
+        
         self.payload["date"] = date
         self.payload["stockNo"] = stockNo
         
@@ -28,14 +36,17 @@ class Stock_Info():
         rawdata = res.json()
 
         if rawdata['stat'] == "OK":
-            #for i in rawdata['data']:
-            #    print i
+            for info in rawdata['data']:
+                self.stockInfo[stockNo].append(info)
+            
             return "success"
         else:
-            print rawdata['stat']
+            #print rawdata['stat']
             return "fail"
         
     def stockTrace(self, stockNo):
+        self.__checkStockNo__(stockNo);
+        
         #Local Variable
         request_date = ""
         parse_result = "success"
@@ -52,7 +63,6 @@ class Stock_Info():
                     request_date = str(year) + str(month) + "01"
                 
                 if request_date < cur_date:
-                    print request_date
                     parse_result = self.stockGet(stockNo, request_date) #Trace this date stock
                 else:
                     parse_result = "success"
@@ -60,13 +70,36 @@ class Stock_Info():
                 if parse_result == "fail":
                     break
             if parse_result == "fail":
+                print "StockNo %s Done" % (stockNo)
                 break
-        
-def main():
+            
+    def __checkStockNo__(self, stockNo):
+        if self.stockInfo.has_key(stockNo) == False:
+            print "Create %s Info" % (stockNo)
+            self.stockInfo[stockNo] = []
+
+def StockRefresh():
+    Infofile_name = "StockNo_ALL.info"
+    Dumpfile_name = "StockInfo"
     stock_url = "http://www.tse.com.tw/exchangeReport/STOCK_DAY"
     payload = {"reponse":"json", "date":"", "stockNo":""}
+
+    #Create stock object
     s = Stock_Info(stock_url, payload)
-    s.stockTrace("0050")
+
+    #Trace all stockNo
+    with open(Infofile_name, 'r') as stockNo_ALL:
+        for raw_stockNo in stockNo_ALL.readlines():
+            stockNo = raw_stockNo.replace("\n",'')
+            if len(stockNo) == 4:
+                s.stockTrace(stockNo)
+
+    #Dump stock info
+    pickle.dump(s.stockInfo, open(Dumpfile_name, 'w'))
     
+def main():
+    
+    StockRefresh()
+
 if __name__=="__main__":
     main()
